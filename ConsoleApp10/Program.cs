2ba8eb2d1d09de8789d2f16722a0d7c9f1a74df7
Admin <Admin@DESKTOP-UGP0C2G>
2024-12-02 13:25:20 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,23 @@
+﻿        int[] array = new int[16];
+        Console.WriteLine("Введите 16 целых чисел:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int countOfReplacements = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 3 == 0)
+            {
+                array[i] = 0;
+                countOfReplacements++;
+            }
+        }
+        Console.WriteLine("Преобразованный массив:");
+foreach (var item in array)
+{
+    Console.Write(item + " ");
+}
+        Console.WriteLine($"\nКоличество замен: {countOfReplacements}");
+    
